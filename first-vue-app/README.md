6ff9233c9b8bc94d2a8f0f844fb17d1a7ca08ad2
Angle-M <98354046+Angle-M@users.noreply.github.com>
2024-02-07 17:40:45 -0500

    re made the project
    Errors were in the project that could not be resolved so to avoid longer troubleshoot time, reverted to a prior state

--- first-vue-app/README.md
+++ first-vue-app/README.md
@@ -1,4 +1,4 @@
-# Starter Project
+# first-vue-app
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ first-vue-app/src/components/MainContent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="main-content">
+        <h1>Interest Title</h1>
+            <p>This would be filler text, but this is where the interest would go</p>
+    </div>
+    <div class="main-content">
+        <h1>Interest Title</h1>
+            <p>This would be filler text, but other interest text here</p>
+    </div>
+    <div class="main-content">
+        <h1>Interest Title</h1>
+            <p>This would be filler text, but other interest text here</p>
+    </div>
+</template>
