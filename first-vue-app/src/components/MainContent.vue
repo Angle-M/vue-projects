141e66f725dacd0786d1cc343b1435a3eab66c95
Angle-M <98354046+Angle-M@users.noreply.github.com>
2024-02-15 20:52:10 -0500

    testing bootstrap to see if module is installed

--- first-vue-app/src/components/MainContent.vue
+++ first-vue-app/src/components/MainContent.vue
@@ -4,6 +4,14 @@
         <h1>Interest Title</h1>
             <p>This would be filler text, but this is where the interest would go</p>
     </div>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
     <div class="main-content">
         <h1>Interest Title</h1>
             <p>This would be filler text, but other interest text here</p>
